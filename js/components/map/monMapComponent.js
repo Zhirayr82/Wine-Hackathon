f41fa3aafe78729fcbd5aedf02f74fe7ce0f86b4
Viateur <viateur.ayegnon@gmail.com>
2017-02-17 01:06:29 +0100

    requete ok

---
+++ js/components/map/monMapComponent.js
@@ -0,0 +1,5 @@
+'use strict';
+angular.module("monMap").component("monMap", {
+	templateUrl: "js/components/monMap/monMapTemplate.html",
+	controller: "monMapController"
+})
---
+++ js/components/map/monMapController.js
@@ -0,0 +1,45 @@
+angular.module('monMap').controller('monMapController', [function(){
+
+
+	var mymap = L.map('mapid', {
+		center:[44.83 , -0.576528],
+		zoom: 17
+		});
+
+	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+		maxZoom: 18,
+		attribution: 'Map data &copy; <a href="httresponse.data.positionp://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+			'<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+			'Imagery © <a href="http://mapbox.com">Mapbox</a>',
+		id: 'mapbox.streets'
+	}).addTo(mymap);
+
+	L.marker([51.5, -0.09]).addTo(mymap)
+		.bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+	L.circle([51.508, -0.11], 500, {
+		color: 'red',response.data.position
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(mymap).bindPopup("I am a circle.");
+
+	L.polygon([
+		[51.509, -0.08],
+		[51.503, -0.06],
+		[51.51, -0.047]
+	]).addTo(mymap).bindPopup("I am a polygon.");
+
+
+	var popup = L.popup();
+
+	function onMapClick(e) {
+		popup
+			.setLatLng(e.latlng)
+			.setContent("You clicked the map at " + e.latlng.toString())
+			.openOn(mymap);
+	}
+
+	mymap.on('click', onMapClick);
+
+
+}])
